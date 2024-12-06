eebfa5f18dcc3c0da5932890f29990a3b4ec16b3
Callum Mansfield <callum@eaglebox.studio>
2024-12-07 09:09:44 +1100

    Adding in game constraints

---
+++ constraints.py
@@ -0,0 +1,14 @@
+# constraints.py
+
+# Screen Dimensions
+WIDTH, HEIGHT = 800, 600
+CELL_SIZE = 20
+
+# Colours
+BLACK = (0,0,0)
+WHITE = (255,255,255,255)
+GREEN = (0,255,0)
+RED = (255,0,0)
+
+# Game Speed
+SPEED = 15
